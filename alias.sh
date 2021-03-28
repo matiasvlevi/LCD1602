ab2e703b01fb0e9f71cdc8201c24c366ff9660eb
matiasvlevi <matiasvlevi@gmail.com>
2021-03-28 16:56:37 -0400

    bashrc write

--- alias.sh
+++ alias.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-alias lcdwrite="sudo python3 /home/pi/LCD1602/src/write.py"
-echo "Successfully set alias lcdwrite"
-alias lcdclear="sudo python3 /home/pi/LCD1602/src/clear.py"
-echo "Successfully set alias lcdclear"
-alias lcdhost="sudo python3 /home/pi/LCD1602/src/hostadress.py"
-echo "Successfully set alias lcdhost"
+sudo echo "alias lcdwrite='sudo python3 /home/pi/LCD1602/src/write.py'" >> /home/pi/.bashrc
+echo 'Successfully set alias lcdwrite'
+
+sudo echo "alias lcdclear='sudo python3 /home/pi/LCD1602/src/clear.py'" >> /home/pi/.bashrc
+echo 'Successfully set alias lcdclear'
+
+sudo echo "alias lcdhost='sudo python3 /home/pi/LCD1602/src/hostadress.py'" >> /home/pi/.bashrc
+echo 'Successfully set alias lcdhost'
