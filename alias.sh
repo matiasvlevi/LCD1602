a1c40f93c8c68dec23a3e43f9b3bcea112a26537
matiasvlevi <matiasvlevi@gmail.com>
2021-03-28 16:59:44 -0400

    alias changes

--- alias.sh
+++ alias.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
+
+#temp
+alias lcdwrite='sudo python3 /home/pi/LCD1602/src/write.py'
 sudo echo "alias lcdwrite='sudo python3 /home/pi/LCD1602/src/write.py'" >> /home/pi/.bashrc
+
 echo 'Successfully set alias lcdwrite'
 
+
+#temp
+alias lcdclear='sudo python3 /home/pi/LCD1602/src/clear.py'
 sudo echo "alias lcdclear='sudo python3 /home/pi/LCD1602/src/clear.py'" >> /home/pi/.bashrc
+
 echo 'Successfully set alias lcdclear'
 
+
+#temp
+alias lcdhost='sudo python3 /home/pi/LCD1602/src/hostadress.py'
 sudo echo "alias lcdhost='sudo python3 /home/pi/LCD1602/src/hostadress.py'" >> /home/pi/.bashrc
+
 echo 'Successfully set alias lcdhost'
