760924b8dd8d5116e85b3463804bfb8505fe8416
matiasvlevi <matiasvlevi@gmail.com>
2021-03-28 16:31:56 -0400

    changes

--- alias.sh
+++ alias.sh
@@ -1,5 +1,6 @@
 alias lcdwrite="sudo python3 /home/pi/LCD1602/src/write.py"
+echo "Successfully set alias lcdwrite"
 alias lcdclear="sudo python3 /home/pi/LCD1602/src/clear.py"
+echo "Successfully set alias lcdclear"
 alias lcdhost="sudo python3 /home/pi/LCD1602/src/hostadress.py"
-echo
-echo "Successfully set aliases lcdwrite, lcdclear and lcdhost."
+echo "Successfully set alias lcdhost"
